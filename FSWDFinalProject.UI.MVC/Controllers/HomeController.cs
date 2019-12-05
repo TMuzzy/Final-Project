c7f383dead418330d0e046a5e1ab036db87ea326
Silver <tristanmuzzy@outlook.com>
2019-12-05 14:30:58 -0600

    Working Contact Form Completed

--- FSWDFinalProject.UI.MVC/Controllers/HomeController.cs
+++ FSWDFinalProject.UI.MVC/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
 ﻿using System.Web.Mvc;
+using FSWDFinalProject.UI.MVC.Models;
+using System;
+using System.Net;
+using System.Net.Mail;
 
 namespace FSWDFinalProject.UI.MVC.Controllers
 {
@@ -22,9 +26,42 @@ namespace FSWDFinalProject.UI.MVC.Controllers
         [HttpGet]
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Contact(ContactViewModel cvm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            string message = $"You have received an email from {cvm.Name} with a subject of '{cvm.Subject}'. Please respond to '{cvm.Email}' with your response to the following message: <br/> {cvm.Message}";
+
+            MailMessage mm = new MailMessage("admin@tmuzzy.com", "tristanmuzzy@outlook.com", cvm.Subject, cvm.Message);
+
+            mm.IsBodyHtml = true;
+            mm.Priority = MailPriority.High;
+            mm.ReplyToList.Add(cvm.Email);
+
+            SmtpClient client = new SmtpClient("mail.tmuzzy.com");
+            client.Credentials = new NetworkCredential("admin@tmuzzy.com", "j@4E8EBNEjq@3@8");
+
+            try
+            {
+                client.Send(mm);
+            }
+            catch (Exception ex)
+            {
+                ViewBag.CustomerMessage = $"We're sorry your request could not be completed at this time. Please try again later. Error Message:<br/>{ex.StackTrace}";
+                return View(cvm);
+            }
+            TempData["EmailConfirm"] = true;
+            TempData["Name"] = cvm.Name;
+            TempData["Email"] = cvm.Email;
+            TempData["Subject"] = cvm.Subject;
+            return RedirectToAction("Contact");
         }
     }
 }
